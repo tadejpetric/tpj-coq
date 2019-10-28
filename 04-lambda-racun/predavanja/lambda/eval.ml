0aa3592eaa118739b0d2292d1548b6cb1380bc7a
Matija Pretnar <matija@pretnar.info>
2019-10-28 10:06:11 +0100

    Poenostavil implementacijo

--- 04-lambda-racun/predavanja/lambda/eval.ml
+++ 04-lambda-racun/predavanja/lambda/eval.ml
@@ -1,3 +1,5 @@
+module S = Syntax
+
 type value =
   | Int of int
   | Bool of bool
@@ -10,54 +12,50 @@ let string_of_value = function
 
 
 let rec eval_exp env = function
-  | Syntax.Var x ->
-      (try
-        List.assoc x env
-      with
-        Not_found -> failwith ("NOT FOUND: "^x))
-  | Syntax.Int n ->
+  | S.Var x -> List.assoc x env
+  | S.Int n ->
       Int n
-  | Syntax.Plus (e1, e2) ->
+  | S.Plus (e1, e2) ->
       let n1 = eval_int env e1
       and n2 = eval_int env e2
       in Int (n1 + n2)
-  | Syntax.Minus (e1, e2) ->
+  | S.Minus (e1, e2) ->
       let n1 = eval_int env e1
       and n2 = eval_int env e2
       in Int (n1 - n2)
-  | Syntax.Times (e1, e2) ->
+  | S.Times (e1, e2) ->
       let n1 = eval_int env e1
       and n2 = eval_int env e2
       in Int (n1 * n2)
-  | Syntax.Bool b ->
+  | S.Bool b ->
       Bool b
-  | Syntax.Equal (e1, e2) ->
+  | S.Equal (e1, e2) ->
       let n1 = eval_int env e1
       and n2 = eval_int env e2
       in Bool (n1 = n2)
-  | Syntax.Less (e1, e2) ->
+  | S.Less (e1, e2) ->
       let n1 = eval_int env e1
       and n2 = eval_int env e2
       in Bool (n1 < n2)
-  | Syntax.Greater (e1, e2) ->
+  | S.Greater (e1, e2) ->
       let n1 = eval_int env e1
       and n2 = eval_int env e2
       in Bool (n1 > n2)
-  | Syntax.IfThenElse (e, e1, e2) ->
+  | S.IfThenElse (e, e1, e2) ->
       let b = eval_bool env e in
       eval_exp env (if b then e1 else e2)
-  | Syntax.Lambda (x, e) ->
+  | S.Lambda (x, e) ->
       let func v =
         let env' = (x, v) :: List.remove_assoc x env in
         eval_exp env' e
       in
       Function func
-  | Syntax.Apply (e1, e2) ->
+  | S.Apply (e1, e2) ->
       let f = eval_fun env e1
       and v = eval_exp env e2
       in
       f v
-  | Syntax.RecLambda (f, x, e) ->
+  | S.RecLambda (f, x, e) ->
       let rec func v =
         let env' = (x, v) :: (f, Function func) :: List.remove_assoc x env in
         eval_exp env' e
@@ -76,74 +74,44 @@ and eval_fun env e =
   | Function f -> f
   | _ -> failwith "Function expected"
 
-let rec subst x v e =
-  match e with
-  | Syntax.Var x' when x = x' -> v
-  | Syntax.Var _ | Syntax.Int _ | Syntax.Bool _ -> e
-  | Syntax.Plus (e1, e2) -> Syntax.Plus (subst x v e1, subst x v e2)
-  | Syntax.Minus (e1, e2) -> Syntax.Minus (subst x v e1, subst x v e2)
-  | Syntax.Times (e1, e2) -> Syntax.Times (subst x v e1, subst x v e2)
-  | Syntax.Equal (e1, e2) -> Syntax.Equal (subst x v e1, subst x v e2)
-  | Syntax.Less (e1, e2) -> Syntax.Less (subst x v e1, subst x v e2)
-  | Syntax.Greater (e1, e2) -> Syntax.Greater (subst x v e1, subst x v e2)
-  | Syntax.IfThenElse (e, e1, e2) -> Syntax.IfThenElse (subst x v e, subst x v e1, subst x v e2)
-  | Syntax.Lambda (x', _) when x = x' -> e
-  | Syntax.Lambda (x', e) -> Syntax.Lambda (x', subst x v e)
-  | Syntax.Apply (e1, e2) -> Syntax.Apply (subst x v e1, subst x v e2)
-  | Syntax.RecLambda (f', x', _) when f' = x' || x = x' -> e
-  | Syntax.RecLambda (f', x', e) -> Syntax.RecLambda (f', x', subst x v e)
-
-exception Stop
+let is_value = function
+  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> true
+  | S.Var _ | S.Plus _ | S.Minus _ | S.Times _ | S.Equal _ | S.Less _ | S.Greater _
+  | S.IfThenElse _ | S.Apply _ -> false
 
 let rec step = function
-  | Syntax.Var _ | Syntax.Int _ | Syntax.Bool _ | Syntax.Lambda _ | Syntax.RecLambda _ -> raise Stop
-  | Syntax.Plus (Syntax.Int n1, Syntax.Int n2) -> Syntax.Int (n1 + n2)
-  | Syntax.Plus (Syntax.Int n1, e2) -> Syntax.Plus (Syntax.Int n1, step e2)
-  | Syntax.Plus (e1, e2) -> Syntax.Plus (step e1, e2)
-  | Syntax.Minus (Syntax.Int n1, Syntax.Int n2) -> Syntax.Int (n1 - n2)
-  | Syntax.Minus (Syntax.Int n1, e2) -> Syntax.Minus (Syntax.Int n1, step e2)
-  | Syntax.Minus (e1, e2) -> Syntax.Minus (step e1, e2)
-  | Syntax.Times (Syntax.Int n1, Syntax.Int n2) -> Syntax.Int (n1 * n2)
-  | Syntax.Times (Syntax.Int n1, e2) -> Syntax.Times (Syntax.Int n1, step e2)
-  | Syntax.Times (e1, e2) -> Syntax.Times (step e1, e2)
-  | Syntax.Equal (Syntax.Int n1, Syntax.Int n2) -> Syntax.Bool (n1 = n2)
-  | Syntax.Equal (Syntax.Int n1, e2) -> Syntax.Equal (Syntax.Int n1, step e2)
-  | Syntax.Equal (e1, e2) -> Syntax.Equal (step e1, e2)
-  | Syntax.Less (Syntax.Int n1, Syntax.Int n2) -> Syntax.Bool (n1 < n2)
-  | Syntax.Less (Syntax.Int n1, e2) -> Syntax.Less (Syntax.Int n1, step e2)
-  | Syntax.Less (e1, e2) -> Syntax.Less (step e1, e2)
-  | Syntax.Greater (Syntax.Int n1, Syntax.Int n2) -> Syntax.Bool (n1 > n2)
-  | Syntax.Greater (Syntax.Int n1, e2) -> Syntax.Greater (Syntax.Int n1, step e2)
-  | Syntax.Greater (e1, e2) -> Syntax.Greater (step e1, e2)
-  | Syntax.IfThenElse (Syntax.Bool b, e1, e2) -> if b then e1 else e2
-  | Syntax.IfThenElse (e, e1, e2) -> Syntax.IfThenElse (step e, e1, e2)
-  | Syntax.Apply (Syntax.RecLambda (f, x, body) as func, arg) ->
-      begin match step_opt arg with
-      | Some arg' -> Syntax.Apply (func, arg')
-      | None ->
-          body
-          |> subst f func
-          |> subst x arg
-      end
-  | Syntax.Apply (Syntax.Lambda (x, body) as func, arg) ->
-      begin match step_opt arg with
-      | Some arg' -> Syntax.Apply (func, arg')
-      | None ->
-          body
-          |> subst x arg
-      end
-  | Syntax.Apply (e1, e2) -> Syntax.Apply (step e1, e2)
-and step_opt e =
-  try Some (step e) with Stop -> None
+  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> failwith "Expected a non-terminal expression"
+  | S.Plus (S.Int n1, S.Int n2) -> S.Int (n1 + n2)
+  | S.Plus (S.Int n1, e2) -> S.Plus (S.Int n1, step e2)
+  | S.Plus (e1, e2) -> S.Plus (step e1, e2)
+  | S.Minus (S.Int n1, S.Int n2) -> S.Int (n1 - n2)
+  | S.Minus (S.Int n1, e2) -> S.Minus (S.Int n1, step e2)
+  | S.Minus (e1, e2) -> S.Minus (step e1, e2)
+  | S.Times (S.Int n1, S.Int n2) -> S.Int (n1 * n2)
+  | S.Times (S.Int n1, e2) -> S.Times (S.Int n1, step e2)
+  | S.Times (e1, e2) -> S.Times (step e1, e2)
+  | S.Equal (S.Int n1, S.Int n2) -> S.Bool (n1 = n2)
+  | S.Equal (S.Int n1, e2) -> S.Equal (S.Int n1, step e2)
+  | S.Equal (e1, e2) -> S.Equal (step e1, e2)
+  | S.Less (S.Int n1, S.Int n2) -> S.Bool (n1 < n2)
+  | S.Less (S.Int n1, e2) -> S.Less (S.Int n1, step e2)
+  | S.Less (e1, e2) -> S.Less (step e1, e2)
+  | S.Greater (S.Int n1, S.Int n2) -> S.Bool (n1 > n2)
+  | S.Greater (S.Int n1, e2) -> S.Greater (S.Int n1, step e2)
+  | S.Greater (e1, e2) -> S.Greater (step e1, e2)
+  | S.IfThenElse (S.Bool b, e1, e2) -> if b then e1 else e2
+  | S.IfThenElse (e, e1, e2) -> S.IfThenElse (step e, e1, e2)
+  | S.Apply (S.Lambda (x, e), v) when is_value v -> S.subst [(x, v)] e
+  | S.Apply (S.RecLambda (f, x, e) as rec_f, v) when is_value v -> S.subst [(f, rec_f); (x, v)] e
+  | S.Apply ((S.Lambda _ | S.RecLambda _) as f, e) -> S.Apply (f, step e)
+  | S.Apply (e1, e2) -> S.Apply (step e1, e2)
 
 let big_step e =
   let v = eval_exp [] e in
   print_endline (string_of_value v)
 
 let rec small_step e =
-  print_endline (Syntax.string_of_exp e);
-  match step_opt e with
-  | Some e' ->
-    print_endline "  -->";
-    small_step e'
-  | None -> ()
+  print_endline (S.string_of_exp e);
+  if not (is_value e) then
+    (print_endline "  -->";
+    small_step (step e))
