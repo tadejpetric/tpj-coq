89c38d89057655307ef55a475ff0323958072ead
Matija Pretnar <matija@pretnar.info>
2019-10-28 11:54:36 +0100

    Semantika velikih korakov dela na izrazih

--- 04-lambda-racun/predavanja/lambda/eval.ml
+++ 04-lambda-racun/predavanja/lambda/eval.ml
@@ -1,78 +1,52 @@
 module S = Syntax
 
-type value =
-  | Int of int
-  | Bool of bool
-  | Function of (value -> value)
 
-let string_of_value = function
-  | Int n -> string_of_int n
-  | Bool b -> if b then "TRUE" else "FALSE"
-  | Function _ -> "FUN"
-
-
-let rec eval_exp env = function
-  | S.Var x -> List.assoc x env
-  | S.Int n ->
-      Int n
+let rec eval_exp = function
+  | S.Var x -> failwith "Expected a closed term"
+  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ as e -> e
   | S.Plus (e1, e2) ->
-      let n1 = eval_int env e1
-      and n2 = eval_int env e2
-      in Int (n1 + n2)
+      let n1 = eval_int e1
+      and n2 = eval_int e2
+      in S.Int (n1 + n2)
   | S.Minus (e1, e2) ->
-      let n1 = eval_int env e1
-      and n2 = eval_int env e2
-      in Int (n1 - n2)
+      let n1 = eval_int e1
+      and n2 = eval_int e2
+      in S.Int (n1 - n2)
   | S.Times (e1, e2) ->
-      let n1 = eval_int env e1
-      and n2 = eval_int env e2
-      in Int (n1 * n2)
-  | S.Bool b ->
-      Bool b
+      let n1 = eval_int e1
+      and n2 = eval_int e2
+      in S.Int (n1 * n2)
   | S.Equal (e1, e2) ->
-      let n1 = eval_int env e1
-      and n2 = eval_int env e2
-      in Bool (n1 = n2)
+      let n1 = eval_int e1
+      and n2 = eval_int e2
+      in S.Bool (n1 = n2)
   | S.Less (e1, e2) ->
-      let n1 = eval_int env e1
-      and n2 = eval_int env e2
-      in Bool (n1 < n2)
+      let n1 = eval_int e1
+      and n2 = eval_int e2
+      in S.Bool (n1 < n2)
   | S.Greater (e1, e2) ->
-      let n1 = eval_int env e1
-      and n2 = eval_int env e2
-      in Bool (n1 > n2)
+      let n1 = eval_int e1
+      and n2 = eval_int e2
+      in S.Bool (n1 > n2)
   | S.IfThenElse (e, e1, e2) ->
-      let b = eval_bool env e in
-      eval_exp env (if b then e1 else e2)
-  | S.Lambda (x, e) ->
-      let func v =
-        let env' = (x, v) :: List.remove_assoc x env in
-        eval_exp env' e
-      in
-      Function func
+      begin match eval_exp e with
+      | S.Bool true -> eval_exp e1
+      | S.Bool false -> eval_exp e2
+      | _ -> failwith "Boolean expected"
+      end
   | S.Apply (e1, e2) ->
-      let f = eval_fun env e1
-      and v = eval_exp env e2
+      let f = eval_exp e1
+      and v = eval_exp e2
       in
-      f v
-  | S.RecLambda (f, x, e) ->
-      let rec func v =
-        let env' = (x, v) :: (f, Function func) :: List.remove_assoc x env in
-        eval_exp env' e
-      in
-      Function func
-and eval_int env e =
-  match eval_exp env e with
-  | Int n -> n
+      begin match f with
+      | S.Lambda (x, e) -> eval_exp (S.subst [(x, v)] e)
+      | S.RecLambda (f, x, e) as rec_f -> eval_exp (S.subst [(f, rec_f); (x, v)] e)
+      | _ -> failwith "Function expected"
+      end
+and eval_int e =
+  match eval_exp e with
+  | S.Int n -> n
   | _ -> failwith "Integer expected"
-and eval_bool env e =
-  match eval_exp env e with
-  | Bool n -> n
-  | _ -> failwith "Boolean expected"
-and eval_fun env e =
-  match eval_exp env e with
-  | Function f -> f
-  | _ -> failwith "Function expected"
 
 let is_value = function
   | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> true
@@ -107,8 +81,8 @@ let rec step = function
   | S.Apply (e1, e2) -> S.Apply (step e1, e2)
 
 let big_step e =
-  let v = eval_exp [] e in
-  print_endline (string_of_value v)
+  let v = eval_exp e in
+  print_endline (S.string_of_exp v)
 
 let rec small_step e =
   print_endline (S.string_of_exp e);
