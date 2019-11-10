dff1cb92fc0a164206e55f7871af0093f19f47ef
Ziga Luksic <ziga.luksic@live.com>
2019-11-10 15:29:09 +0100

    popravi pretty printing

--- 04-lambda-racun/lambda/syntax.ml
+++ 04-lambda-racun/lambda/syntax.ml
@@ -40,29 +40,25 @@ let rec subst sbst = function
       RecLambda (f, x, subst sbst' e)
   | Apply (e1, e2) -> Apply (subst sbst e1, subst sbst e2)
 
-let rec string_of_exp5 = function
+let rec string_of_exp3 = function
   | IfThenElse (e, e1, e2) ->
-      "IF " ^ string_of_exp4 e ^ " THEN " ^ string_of_exp4 e1 ^ " ELSE " ^ string_of_exp5 e2
+      "IF " ^ string_of_exp2 e ^ " THEN " ^ string_of_exp2 e1 ^ " ELSE " ^ string_of_exp3 e2
   | Lambda (x, e) ->
-      "FUN " ^ x ^ " -> " ^ string_of_exp5 e
+      "FUN " ^ x ^ " -> " ^ string_of_exp3 e
   | RecLambda (f, x, e) ->
-      "REC " ^ f ^ " " ^ x ^ " -> " ^ string_of_exp5 e
-  | e -> string_of_exp4 e
-and string_of_exp4 = function
-  | Equal (e1, e2) ->
-    string_of_exp3 e1 ^ " = " ^ string_of_exp3 e2
-  | Less (e1, e2) ->
-    string_of_exp3 e1 ^ " < " ^ string_of_exp3 e2
-  | Greater (e1, e2) ->
-    string_of_exp3 e1 ^ " > " ^ string_of_exp3 e2
-  | e -> string_of_exp3 e
-and string_of_exp3 = function
-  | Plus (e1, e2) ->
-    string_of_exp2 e1 ^ " + " ^ string_of_exp2 e2
-  | Minus (e1, e2) ->
-    string_of_exp2 e1 ^ " - " ^ string_of_exp2 e2
+      "REC " ^ f ^ " " ^ x ^ " -> " ^ string_of_exp3 e
   | e -> string_of_exp2 e
 and string_of_exp2 = function
+  | Equal (e1, e2) ->
+    string_of_exp1 e1 ^ " = " ^ string_of_exp1 e2
+  | Less (e1, e2) ->
+    string_of_exp1 e1 ^ " < " ^ string_of_exp1 e2
+  | Greater (e1, e2) ->
+    string_of_exp1 e1 ^ " > " ^ string_of_exp1 e2
+  | Plus (e1, e2) ->
+    string_of_exp1 e1 ^ " + " ^ string_of_exp1 e2
+  | Minus (e1, e2) ->
+    string_of_exp1 e1 ^ " - " ^ string_of_exp1 e2
   | Times (e1, e2) ->
     string_of_exp1 e1 ^ " * " ^ string_of_exp1 e2
   | e -> string_of_exp1 e
@@ -74,6 +70,6 @@ and string_of_exp0 = function
   | Int n -> string_of_int n
   | Bool b -> if b then "TRUE" else "FALSE"
   | Var x -> x
-  | e -> "(" ^ string_of_exp5 e ^ ")"
+  | e -> "(" ^ string_of_exp3 e ^ ")"
 
 let string_of_exp = string_of_exp5
